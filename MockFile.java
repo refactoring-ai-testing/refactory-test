cd03f7c87ef640f7ef06c965ad1963e0b10899bf
David van der Leij <davidvanderleij@gmail.com>
2021-08-06 10:49:19 +0200

    Add example of refactor candidate and non-refactor candidate

--- MockFile.java
+++
@@ -1,5 +0,0 @@
-package mockproject;
-
-public class MockFile {
-    
-}
---
+++ MockJavaFileRefactor.java
@@ -0,0 +1,171 @@
+package mockproject;
+
+public class MockJavaFileRefactor {
+    public void veryComplicatedMethodThatRequiresExtractMethod() {
+        var x = true;
+        var y = false;
+        if (x) {
+            x = y;
+            y = x;
+            y = y;
+            x = y = x = y = y = y = x;
+            if (y) {
+                while (x == y) {
+                    if (x) {
+                        x = y;
+                        y = x;
+                        y = y;
+                        x = y = x = y = y = y = x;
+                        if (y) {
+                            while (x == y) {
+                                if (x) {
+                                    x = y;
+                                    y = x;
+                                    y = y;
+                                    x = y = x = y = y = y = x;
+                                    if (y) {
+                                        while (x == y) {
+                                            if (x) {
+                                                x = y;
+                                                y = x;
+                                                y = y;
+                                                x = y = x = y = y = y = x;
+                                                if (y) {
+                                                    while (x == y) {
+                                                        if (x) {
+                                                            x = y;
+                                                            y = x;
+                                                            y = y;
+                                                            x = y = x = y = y = y = x;
+                                                            if (y) {
+                                                                while (x == y) {
+                                                                    if (x) {
+                                                                        x = y;
+                                                                        y = x;
+                                                                        y = y;
+                                                                        x = y = x = y = y = y = x;
+                                                                        if (y) {
+                                                                            while (x == y) {
+                                                                                if (x) {
+                                                                                    x = y;
+                                                                                    y = x;
+                                                                                    y = y;
+                                                                                    x = y = x = y = y = y = x;
+                                                                                    if (y) {
+                                                                                        while (x == y) {
+                                                                                            if (x) {
+                                                                                                x = y;
+                                                                                                y = x;
+                                                                                                y = y;
+                                                                                                x = y = x = y = y = y = x;
+                                                                                                if (y) {
+                                                                                                    while (x == y) {
+
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                        if (x) {
+                                                            x = y;
+                                                            y = x;
+                                                            y = y;
+                                                            x = y = x = y = y = y = x;
+                                                            if (y) {
+                                                                while (x == y) {
+                                                                    if (x) {
+                                                                        x = y;
+                                                                        y = x;
+                                                                        y = y;
+                                                                        x = y = x = y = y = y = x;
+                                                                        if (y) {
+                                                                            while (x == y) {
+                                                                                if (x) {
+                                                                                    x = y;
+                                                                                    y = x;
+                                                                                    y = y;
+                                                                                    x = y = x = y = y = y = x;
+                                                                                    if (y) {
+                                                                                        while (x == y) {
+                                                                                            if (x) {
+                                                                                                x = y;
+                                                                                                y = x;
+                                                                                                y = y;
+                                                                                                x = y = x = y = y = y = x;
+                                                                                                if (y) {
+                                                                                                    while (x == y) {
+
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                        if (x) {
+                                                            x = y;
+                                                            y = x;
+                                                            y = y;
+                                                            x = y = x = y = y = y = x;
+                                                            if (y) {
+                                                                while (x == y) {
+                                                                    if (x) {
+                                                                        x = y;
+                                                                        y = x;
+                                                                        y = y;
+                                                                        x = y = x = y = y = y = x;
+                                                                        if (y) {
+                                                                            while (x == y) {
+                                                                                if (x) {
+                                                                                    x = y;
+                                                                                    y = x;
+                                                                                    y = y;
+                                                                                    x = y = x = y = y = y = x;
+                                                                                    if (y) {
+                                                                                        while (x == y) {
+                                                                                            if (x) {
+                                                                                                x = y;
+                                                                                                y = x;
+                                                                                                y = y;
+                                                                                                x = y = x = y = y = y = x;
+                                                                                                if (y) {
+                                                                                                    while (x == y) {
+
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
